0bec18f8ec094e6e6d6d32ec0d4c4182565f4171
OM Miles <miles@ommiles.com>
2021-05-19 12:03:07 -0700

    commit tree 7dc98b54a387040d2cb7887fbde8552d38d5d710

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+class Dog
+
+    @@all=[]
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name=name
+        # save gets called inside initialize when a new Dog is created
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        # clears all dogs from array
+        @@all=[]
+    end
+
+    def self.print_all
+        # prints dogs names
+        @@all.map {|dog| puts dog.name }
+    end
+
+    def save
+        #save adds this dog instance to the @@all array when called
+        @@all << self
+    end
+end
